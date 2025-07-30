e9ddaf6dd6d856a81102155a224af825c73be594
Ishika <144527060+Ishika234@users.noreply.github.com>
2025-07-30 22:51:34 +0530

    Create 04-Literals.java

---
+++ Basics/04-Literals.java
@@ -0,0 +1,26 @@
+public class Literals {
+
+	public static void main(String[] args) {		
+		/*int num = 5;
+		System.out.println(num);
+		
+		int num = 585;
+		System.out.println(num);
+		
+		int num = 0b101;
+		System.out.println(num);
+		
+		int num = 0x7E;
+		System.out.println(num);
+		
+		int num = 10_00_00_000;
+		System.out.println(num);*/
+		
+		double num = 56;
+		System.out.println(num);
+		
+		char c = 'a';
+		c++;
+		System.out.println(c);		 
+	}
+}
