938efd4afc5d544aad3803183b501093091ab170
Ishika <144527060+Ishika234@users.noreply.github.com>
2025-07-30 22:00:56 +0530

    Create DataTypes-03.java

---
+++ Basics/DataTypes-03.java
@@ -0,0 +1,22 @@
+public class DataTypes-03 {
+
+	public static void main(String[] args) {
+		
+	// Primitive DataTypes
+        byte byteNum = 10;               
+        short shortNum = 100;           
+        int intNum = 567;              
+        long longNum = 105678L;        
+        
+        float floatNum = 5.75f;          
+        double doubleNum = 19.99;
+
+        char letter = 'A'; 
+        boolean isJavaFun = true;
+        
+        System.out.println("byte: " + byteNum);
+        System.out.println("short: " + shortNum);
+        System.out.println("int: " + intNum);
+        System.out.println("long: " + longNum);
+  }
+}
